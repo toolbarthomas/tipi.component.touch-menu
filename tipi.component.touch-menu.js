54cda30c4f51ea198e40046f6666c29f75e1048d
Thomas van der Velde <contact@toolbarthomas.net>
2017-03-07 17:25:55 +0100

    Close drawer when related toggle is not visible

--- tipi.component.touch-menu.js
+++ tipi.component.touch-menu.js
@@ -20,7 +20,8 @@
 	};
 
 
-	window.setTouchMenu = function() {
+	window.setTouchMenu = function()
+	{
 		var touch_menu_toggle = $('.' + data.elements.toggle).not(data.states.toggle_ready);
 
 		$(document).on({
@@ -32,6 +33,22 @@
 			},
 			'tipi.touchMenu.close' : function(event, elements) {
 				closeTouchMenu(elements);
+			},
+			'tipi.touchMenu.resize' : function(event, elements) {
+				checkTouchMenuToggleVisibility(elements);
+			}
+		});
+
+		var resize;
+		$(window).on({
+			resize : function()
+			{
+				clearTimeout(resize);
+				resize = setTimeout(function() {
+					$(document).trigger('tipi.touchMenu.resize', [{
+						toggles : touch_menu_toggle
+					}]);
+				}, 250);
 			}
 		});
 
@@ -52,30 +69,27 @@
 					event.preventDefault();
 
 					var toggle = $(this);
+					var toggles = getRelatedTouchMenuToggles(touch_menu_toggle, toggle);
 
-					var toggles = toggle.data(data.attributes.related_toggles);
-					if(typeof toggles === 'undefined')
+					if(toggles.length === 0)
 					{
-						toggles = getRelatedTouchMenuToggles(touch_menu_toggle, toggle);
-
-						toggle.data(data.attributes.related_toggles, toggles);
+						return;
 					}
 
 					if(toggle.hasClass(data.states.toggle_active))
 					{
 						$(document).trigger('tipi.touchMenu.close', [{
-							toggle : toggles,
+							toggles : toggles,
 							drawer : drawer
 						}]);
 					}
 					else
 					{
 						$(document).trigger('tipi.touchMenu.open', [{
-							toggle : toggles,
+							toggles : toggles,
 							drawer : drawer
 						}]);
 					}
-
 				}
 			});
 
@@ -86,7 +100,8 @@
 		});
 	}
 
-	function initTouchMenu(elements) {
+	function initTouchMenu(elements)
+	{
 		if(typeof elements === 'undefined')
 		{
 			return;
@@ -96,56 +111,103 @@
 		elements.drawer.addClass(data.states.drawer_ready);
 	}
 
-	function openTouchMenu(elements) {
+	function openTouchMenu(elements)
+	{
 		if(typeof elements === 'undefined')
 		{
 			return;
 		}
 
-		elements.toggle.addClass(data.states.toggle_active);
+		elements.toggles.addClass(data.states.toggle_active);
 		elements.drawer.addClass(data.states.drawer_active);
 	}
 
-	function closeTouchMenu(elements) {
+	function closeTouchMenu(elements)
+	{
 		if(typeof elements === 'undefined')
 		{
 			return;
 		}
 
-		elements.toggle.removeClass(data.states.toggle_active);
+		elements.toggles.removeClass(data.states.toggle_active);
 		elements.drawer.removeClass(data.states.drawer_active);
+	}
+
+	function checkTouchMenuToggleVisibility(elements)
+	{
+		if(typeof elements === 'undefined')
+		{
+			return;
+		}
+
+		elements.toggles.each(function() {
+			var toggle = $(this);
+			var toggles = getRelatedTouchMenuToggles(elements.toggles, toggle);
+			var drawer = getTouchMenuDrawer(toggle);
+
+			if(drawer.length === 0 || toggles.length === 0)
+			{
+				return;
+			}
+
+			var visible = false;
+			toggles.each(function() {
+				if($(this).is(':visible'))
+				{
+					visible = true;
+				}
+			});
+
+
+			if(visible)
+			{
+				return;
+			}
+
+			$(document).trigger('tipi.touchMenu.close', [{
+				toggles : toggles,
+				drawer : drawer
+			}]);
+		});
 	}
 
 	function getRelatedTouchMenuToggles(toggles, toggle)
 	{
-		//Create an empty jQuery object where we can add the related toggles
-		var related_toggles = $();
-
-		//Get the registered drawer we've added previously
-		var registered_drawer = toggle.data(data.attributes.registered_drawer);
-		if(typeof registered_drawer === 'undefined')
+		var related_toggles = toggle.data(data.attributes.related_toggles);
+		if(typeof related_toggles === 'undefined')
 		{
-			related_toggles = related_toggles.add(toggle);
-		}
-		else
-		{
-			toggles.each(function() {
-				var current_toggle = $(this);
-				var current_registered_drawer = current_toggle.data(data.attributes.registered_drawer);
+			//Create an empty jQuery object where we can add the related toggles
+			related_toggles = $();
 
-				if(!registered_drawer.is(current_registered_drawer))
-				{
-					return;
-				}
+			//Get the registered drawer we've added previously
+			var registered_drawer = toggle.data(data.attributes.registered_drawer);
+			if(typeof registered_drawer === 'undefined')
+			{
+				related_toggles = related_toggles.add(toggle);
+			}
+			else
+			{
+				toggles.each(function() {
+					var current_toggle = $(this);
+					var current_registered_drawer = current_toggle.data(data.attributes.registered_drawer);
 
-				related_toggles = related_toggles.add(current_toggle);
-			});
+					if(!registered_drawer.is(current_registered_drawer))
+					{
+						return;
+					}
+
+					related_toggles = related_toggles.add(current_toggle);
+				});
+			}
+
+			toggle.data(data.attributes.related_toggles, related_toggles);
 		}
 
 		return related_toggles;
 	}
 
-	function getTouchMenuDrawer(toggle) {
+	function getTouchMenuDrawer(toggle)
+	{
 		if(typeof toggle === 'undefined')
 		{
 			return;
